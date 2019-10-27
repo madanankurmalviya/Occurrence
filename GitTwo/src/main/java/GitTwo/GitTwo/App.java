ff06e99cc4688f918fcd5ced2397d556ff880d2d
ANKUR MALVIYA <56480932+madanankurmalviya@users.noreply.github.com>
2019-10-27 13:40:14 +0530

    Git

---
+++ GitTwo/src/main/java/GitTwo/GitTwo/App.java
@@ -0,0 +1,13 @@
+package GitTwo.GitTwo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
